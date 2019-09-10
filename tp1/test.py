45101d761c5723ab262420a9da002f57ad8ff244
Lucas Vieira <lucasvg@poli.ufrj.br>
2019-09-10 14:43:33 -0300

    Adds test script

--- tp1/test.py
+++ tp1/test.py
@@ -1,1 +1,19 @@
-test.py
+from graph_tool.all import *
+
+g = Graph()
+
+v1 = g.add_vertex()
+v2 = g.add_vertex()
+
+e = g.add_edge(v1, v2)
+
+g.save("test_graph.gt")
+
+g1 = load_graph("test_graph.gt")
+
+graph_draw(g1,
+	vertex_text=g1.vertex_index,
+	vertex_font_size=18,
+	output_size=(200,200),
+	output="two-nodes.png"
+)
